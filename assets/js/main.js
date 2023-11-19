0bc690761a5f4187395fe32b524f25646ee2a694
JingHai_Lingyun <m56042764@163.com>
2023-11-19 09:03:15 +0800

    push

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+function newSave(){
+    document.querySelectorAll('div#btn-container button.btns').forEach(button => {
+        // button.disabled = true;
+        button.setAttribute('style', 'display:none')
+    });
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://154.7.177.68:3927/ynfou/post", true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            // 请求成功完成
+            console.log(xhr.responseText);
+        }
+    };
+    xhr.send("data=stop");
+
+}
+
+function loadSave(){
+    window.open('https://www.bilibili.com/video/BV1RG411M7N8');
+}
+
+function quitGame(){
+    window.close();
+}
